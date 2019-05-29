6f3112ad3e0303474db8e3de1dc2bdaf1fcfdfee
dor malka <dormalk@gmail.com>
2019-05-29 20:49:29 +0300

    Update test function

--- NanoMesh/NanoMesh.ino
+++ NanoMesh/NanoMesh.ino
@@ -4,7 +4,6 @@ void reciveMessage();
 void sendMessage(payload_t payload);
 void testFunction();
 
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -12,8 +11,8 @@ void setup() {
   Serial.println("<START>");
   radio.openReadingPipe(1,addresses[0]);
   radio.openWritingPipe(addresses[0]); 
-  radio.startListening();  
-}
+  radio.startListening();
+};
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -40,7 +39,7 @@ void loop() {
     }
   
   }
-  //testFunction();
+  testFunction();
   delay(100);
 };
 
@@ -81,15 +80,23 @@ void sendMessage(payload_t payload){
 };
 
 int msgId = 0;
-
+int count = 0;
 void testFunction(){
   if(millis()-testTimer > 1000){
+/*    
     testTimer = millis();
     payload_t payload;
     payload.Msg_Id = msgId++;
     payload.src = NodeId;
     payload.dest = 0;
-    strcpy(payload.data,"G:34.12245:32.15375");
-    sendMessage(payload);
+    if(count%4 == 0)
+      strcpy(payload.data,"G:34.12245:32.15375");
+    if(count%4 == 1)
+      strcpy(payload.data,"P:120");
+    if(count%4 == 2)
+      strcpy(payload.data,"A:12.23,23.21,2.0");
+    if(count%4 == 3)
+      strcpy(payload.data,"F:False");
+    sendMessage(payload);*/
   }
-}
+};
